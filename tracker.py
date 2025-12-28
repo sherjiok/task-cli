19895c86dfd9a09142f8dcbc4891e0aa19a082c2
Sergio <157196660+sherjiok@users.noreply.github.com>
2025-12-27 21:03:24 -0300

    fixed id logic, add error handling and help func

--- tracker.py
+++ tracker.py
@@ -6,54 +6,60 @@ import datetime
 STATUS_LIST = ["todo","in-progress","done"]
 DB_FILE = "tasks.json"
 
-#TODO: fix id logic, use argprse, handle errors (try/catch), validate sys.argv arguments, 
-
 def write_json(data):    
-    with open(DB_FILE,'w') as json_file:
-        return json.dump(data, json_file, indent=4)
+    try:
+        with open(DB_FILE,'w') as json_file:
+            return json.dump(data, json_file, indent=4)
+    except FileNotFoundError:
+        return
+    except PermissionError:
+        print("Error: No tengo permisos para escribir en el archivo.")
+        return
+    except IOError as e:
+        print(f"Error de escritura: {e}")    
+        return
 
 def read_json():
-    if not os.path.exists(DB_FILE):
+    try:
+        with open(DB_FILE,'r')as json_file:
+            return json.load(json_file)
+    except FileNotFoundError:
         return []
-    with open(DB_FILE,'r')as json_file:
-        return json.load(json_file)
+    except json.JSONDecodeError:
+        return []
+        
 
 def find_task(data, id):
+    try:
+        target_id = int(id)
+    except ValueError:
+        return
     for task in data:
-        if task["id"] == int(id):
+        if task["id"] == target_id:
             return task
     return None
     
-def add_task():    
+def add_task(description):    
     data = read_json()   
-
-    task = create_task(data)
-    if task == None:
-        print("You must add a task description")
-        return
+    
+    task = create_task(data, description)
     
     data.append(task)    
     
     write_json(data)
-    print(f"Task added succesfully (ID: {data[-1]["id"] })") 
-    return
+    return task["id"]
 
-def create_task(data):
-    if len(sys.argv) < 3:
-        return    
-    
+def create_task(data, description):
     if not data:
         new_id = 1
     else:
-        last_id = data[-1]["id"]
-        new_id = last_id +1
-    
-    description = " ".join(sys.argv[2:])
+        max_id = max(task["id"] for task in data)
+        new_id = max_id + 1
     
     task = {
         "id": new_id,
         "description": description,
-        "status": STATUS_LIST[0],
+        "status": "todo",
         "createdAt": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "updatedAt": None
     }
@@ -62,21 +68,18 @@ def create_task(data):
 
 def update_task(id, new_data):
     
-    if len(sys.argv) < 4:
-        print("Usage: task-cli update <id> <new_description>")
-        return    
     data = read_json()
     
     task = find_task(data, id)    
     
     if not task:
         print(f"Task not found")
-        return
+        return False
     
     task["description"] = new_data
     task["updatedAt"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     write_json(data)
-    print(f"Task updated succesfully") 
+    return True
 
 def remove_task(id):
     data = read_json()
@@ -85,11 +88,11 @@ def remove_task(id):
     
     if not task:
         print("Task not found")
-        return
+        return False
     
     data.remove(task)
-    print("Task removed succesfully")
-    write_json(data) 
+    write_json(data)
+    return True 
 
 def list_tasks():
     data = read_json()
@@ -100,9 +103,6 @@ def list_tasks():
         print(json_file.read())
         
 def list_by_status(status):
-    if status not in STATUS_LIST:
-        print("invalid status: todo, in progress, done")
-        return
     data = read_json()
     filtered = []
     for i in data:
@@ -110,43 +110,93 @@ def list_by_status(status):
             filtered.append(i)
     print(filtered)
 
-def update_status(new_status, id):
-    if new_status not in STATUS_LIST:
-        print("invalid status: todo, in-progress, done")
-        return    
+def update_status(new_status, id): 
     data = read_json()
     
     task = find_task(data, id)
     
     if not task:
         print("Task not found")
-        return
-        
+        return False
     task["status"] = new_status 
     task["updatedAt"] =  datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")  
+    
     write_json(data)
     
-    return
+    return True
+
+def show_help():
+    print("""
+    Task Manager CLI
+        
+    Commands:
+        add <description>           Add a new task
+        update <id> <description>   Update task description
+        remove <id>                 Remove a task
+        list                        List all tasks
+        list [status]               List all tasks or filter by status
+        mark <status> <id>          Change task status
+        """)
         
 def main():
-    if len(sys.argv) < 2:
-        return
-    if sys.argv[1] == 'add':
-        add_task()
-    elif sys.argv[1] == 'update':
-        update = " ".join(sys.argv[3:])
-        update_task(sys.argv[2], update)
-    elif sys.argv[1] == 'remove':
-        remove_task(sys.argv[2])
-    elif sys.argv[1] == 'list' and len(sys.argv) < 3:
-        list_tasks()
-    elif sys.argv[1] == 'list':
-        list_by_status(sys.argv[2])
-    elif sys.argv[1] == "mark":
-        update_status(sys.argv[2], sys.argv[3])
-    else:
+    if len(sys.argv) < 2 or sys.argv[1] in ['help', '-h', '--help']:
+        show_help()
         return
     
+    command = sys.argv[1].lower()
+    
+    try:
+        if command == "add":
+            if len(sys.argv) < 3:
+                print("Usage: tracker.py add <task_description>")
+                return
+            description = " ".join(sys.argv[2:])
+            new_id = add_task(description)
+            print(f"Task added successfully (ID: {new_id})")
+            
+        elif command == "list":
+            if len(sys.argv) >= 3:
+                status = sys.argv[2]
+                if status not in STATUS_LIST:
+                    print("Invalid Status: todo, in-progress, done")
+                    return
+                else:
+                    list_by_status(status)
+            else:
+                list_tasks()
+        elif command == "update":
+            if len(sys.argv) < 4:
+                print("Usage: tracker.py update <task_id> <new_description>")
+                return
+            
+            task_id = int(sys.argv[2])
+            new_description = " ".join(sys.argv[3:])
+            
+            if update_task(task_id, new_description):
+                print("Task updated succesfully") 
+        elif command == "remove":
+            if len(sys.argv) < 3:
+                print("Usage: tracker.py remove <task_id>")
+                return
+            else:
+                task_id = sys.argv[2]
+                if remove_task(task_id):
+                    print("Task removed succesfully") 
+        elif command == "mark":
+            if len(sys.argv) < 4:
+                print("Usage: tracker.py mark <task_status> <task_id>")
+            else:
+                new_status = sys.argv[2]
+                task_id = sys.argv[3]
+                if update_status(new_status, task_id):
+                    print("Status updated succesfully")
+        else:
+            print(f"Unknown command: {command}\nUse -h, --help, help for help")
+    except ValueError:
+        print("Error: ID must be a number.")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+            
 main()
 
 
